991ec051ebb36eec5eb81d3b3b89087f3366f942
Deskillz Games <js@deskillz.games>
2025-03-31 20:10:57 -0400

    seo commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useState } from "react";
+import Swal from "sweetalert2";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title:"Lim Lim Cubes | Contact Me",
+  description: "For any inquires or concerns contact me here!",
+  keywords:"limcubes, speedcubing, Rubik's Cube, puzzle solving, cube algorithms, cubing tutorials, CFOP method, speedsolving, WCA, cube notation, cubing tips, best speedcubes, 3x3, 2x2, 4x4, 5x5, Megaminx, Pyraminx, Skewb, cubing competitions, contact, fastest cubers, cube reviews, cube mods, advanced cubing techniques, PLL, OLL, F2L, beginner cubing guide, cube timers, online cubing, cubing tricks, Lim Cubes",
+
+};
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      Swal.fire({
+        title: "Email Sent!",
+        text: "I'll get back to you as soon as possible.",
+        icon: "success",
+      });
+      const groups = document.querySelectorAll(".form-group");
+      groups.forEach((group) => {
+        const input = group.querySelector("input") as HTMLInputElement;
+        const textarea = group.querySelector("textarea") as HTMLTextAreaElement;
+        if (input) input.value = "";
+        if (textarea) textarea.value = "";
+      });
+    } else {
+      Swal.fire({
+        title: "Error",
+        text: "Email failed to send. Please try again later.",
+        icon: "error",
+      });
+    }
+  };
+
+  return (
+    <main>
+      <title>Contact Me - Lim Cubes</title>
+      <section className="contact">
+        <h2>Contact Me</h2>
+        <p>Feel free to reach out to me for any inquiries or questions.</p>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              required
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default ContactPage;
---
+++ app/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useEffect } from "react";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title:"Lim Lim Cubes | Home",
+  description: "Learn how to solve the rubiks cube easier with Lim Cubes! We've got tons of resources and tutorials that can help and cater to your needs regardless of your skill level!",
+  keywords:"limcubes, speedcubing, Rubik's Cube, puzzle solving, cube algorithms, cubing tutorials, CFOP method, speedsolving, WCA, cube notation, cubing tips, best speedcubes, 3x3, 2x2, 4x4, 5x5, Megaminx, Pyraminx, Skewb, cubing competitions, home, fastest cubers, cube reviews, cube mods, advanced cubing techniques, PLL, OLL, F2L, beginner cubing guide, cube timers, online cubing, cubing tricks, Lim Cubes",
+};
+
+export default function Home() {
+  useEffect(() => {
+    const preloader = document.getElementById("preloader");
+    if (!preloader) return;
+
+      setTimeout(function () {
+        preloader.style.opacity = "0";
+        setTimeout(function () {
+          preloader.style.display = "none";
+        }, 500);
+      }, 1200);
+  }, []);
+
+  return (
+    <main>
+      <title>Home - Lim Cubes</title>
+      {/* Preloader */}
+      <div id="preloader">
+        <div className="cube">
+          <div className="face front"></div>
+          <div className="face back"></div>
+          <div className="face right"></div>
+          <div className="face left"></div>
+          <div className="face top"></div>
+          <div className="face bottom"></div>
+        </div>
+      </div>
+
+      {/* Hero Section */}
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Welcome to Lim Cubes</h1>
+          <p>Learn, grow, and pursue your cubing journey!</p>
+          <a href="/about" className="cta-button">
+            Learn More!
+          </a>
+        </div>
+      </section>
+
+      {/* Introduction Section */}
+      <section className="intro">
+        <h2>About Me</h2>
+        <p>
+          Hi, I&apos;m Liam, a passionate content creator. I specialize in
+          cubing and video production, as well as helping people achieve their
+          goals with the Rubik&apos;s Cube through one-on-one coaching, video
+          tutorials, and providing resources so they don&apos;t have to figure
+          it all out on their own.
+        </p>
+      </section>
+
+      {/* Featured Content Section */}
+      <section className="featured">
+        <h2>What I Offer</h2>
+        <div className="cards">
+          <div className="card">
+            <h3>Personalized Coaching</h3>
+            <p>One-on-one sessions tailored to your learning needs.</p>
+            <a href="https://www.fiverr.com/s/xXmwqpq" className="card-button">
+              Learn More
+            </a>
+          </div>
+          <div className="card">
+            <h3>YouTube Channel</h3>
+            <p>Free tutorials and tips on how to cube.</p>
+            <a
+              href="https://youtube.com/@limcubes"
+              target="_blank"
+              className="card-button"
+            >
+              Watch Now
+            </a>
+          </div>
+          <div className="card">
+            <h3>Other Resources</h3>
+            <p>
+              Here are some useful resources to help you on your cubing journey.
+            </p>
+            <a href="/otherresources" className="card-button">
+              Explore Resources
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
